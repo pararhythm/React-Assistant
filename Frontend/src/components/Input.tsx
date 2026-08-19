5007ab26b24046b055e7ea94bf16883475a4bd2a
pararhythm <pararhythm.lab@gmail.com>
2026-08-19 20:54:32 +0700

    Feat: Added the Home Screen and React Router

---
+++ Frontend/src/components/Input.tsx
@@ -0,0 +1,5 @@
+export default function Input() {
+  <div className="bg-[#2D2C2C]">
+    <h3>Explain useEffect like I'm five years old</h3>
+  </div>;
+}
---
+++ Frontend/src/pages/Home.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div
+      className="bg-[radial-gradient(circle_at_center,#3F3D3D_0%,#1C1C1C_100%)] w-screen h-screen
+      flex
+      flex-col
+      justify-center
+      items-center"
+    >
+      <h2 className="text-[#EDEDED] font-bold text-3xl text-center">
+        What are you working on today ?
+      </h2>
+    </div>
+  );
+}
