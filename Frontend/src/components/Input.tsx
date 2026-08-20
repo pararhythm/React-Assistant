1ef327b20e4a008c3d1e188856e1e0e4271e09ff
pararhythm <pararhythm.lab@gmail.com>
2026-08-20 20:54:04 +0700

    feat: Added the input bar to the Home screen

--- Frontend/src/components/Input.tsx
+++ Frontend/src/components/Input.tsx
@@ -1,5 +1,29 @@
+import { InputBase } from '@mui/material';
+
 export default function Input() {
-  <div className="bg-[#2D2C2C]">
-    <h3>Explain useEffect like I'm five years old</h3>
-  </div>;
+  return (
+    <div className="bg-[#2D2C2C] w-4xl min-h-36 border-2 rounded-2xl border-[#3b3b3b] flex flex-col justify-between p-4">
+      <InputBase
+        multiline
+        className="w-full"
+        placeholder="Explain useEffect like I'm 5 years old"
+        slotProps={{
+          input: {
+            sx: {
+              fontSize: 16,
+              color: '#FFFFFF',
+              '&::placeholder': {
+                color: '#9CA3AF', // Change color
+                opacity: 0.5, // Override MUI's default lower opacity
+              },
+            },
+          },
+        }}
+      />
+      <div className="input-toolbar">
+        <div className="left-toolbar"></div>
+        <div className="right-toolbar"></div>
+      </div>
+    </div>
+  );
 }
--- Frontend/src/pages/Home.tsx
+++ Frontend/src/pages/Home.tsx
@@ -1,3 +1,5 @@
+import Input from '../components/Input';
+
 export default function Home() {
   return (
     <div
@@ -5,11 +7,13 @@ export default function Home() {
       flex
       flex-col
       justify-center
-      items-center"
+      items-center
+      gap-5"
     >
       <h2 className="text-[#EDEDED] font-bold text-3xl text-center">
         What are you working on today ?
       </h2>
+      <Input />
     </div>
   );
 }
